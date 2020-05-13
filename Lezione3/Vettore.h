21f6eea88d305f19381caff41b514c8bf0af16ab
Edo Draetta <edoardoluigi.draetta@studenti.unimi.it>
2020-05-13 15:38:57 -0500

    Init Template Class Vettore

---
+++ Lezione3/Vettore.h
@@ -0,0 +1,96 @@
+#ifndef __Vettore_h__
+#define __Vettore_h__
+
+// definizione e implementazione perche' oggetto template
+
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+template <typename T> class Vettore {
+
+public:
+
+	Vettore(){
+		// costruttore default
+		m_N = 0 
+		m_v = NULL;
+	};
+
+	Vettore(unsigned int size){
+		// overloaded constructor
+		m_N = size;
+		m_v = new T[size]; // create an array
+		for (int i = 0; i < m_N; i++){
+			m_v[0] = 0;
+		}
+	};
+
+
+	Vettore(const Vettore& V){
+		// costruttore copy
+		m_N = V.GetN();
+		m_v = new T[size];
+		for (int i = 0; i < m_N; i++){
+			m_v[i] = V.GetComponent(i);
+		}
+	}; 
+
+	Vettore& operator=(const Vettore& V){
+
+		m_N = V.GetN();
+		if (m_v) delete m_v;
+		m_v = new T[m_N];
+		for (int i = 0; i < m_N; i++){
+			return *this; 
+			// "this" is pointer to the object to which the method belongs
+		}
+	}; 
+
+	~Vettore(){delete[] m_v;};
+
+	unsigned int GetN() const {return m_N;}; 
+
+	void SetComponent(unsigned int i, T value){
+		if (i<m_N){
+			m_v[i] = value;
+		} else {
+			cerr << "Errore : indice " << i << ", dimensione " << m_N << endl;
+			exit(-1);
+		}
+	};
+
+	T GetComponent(unsigned int i) const{
+		if (i<m_N) {
+			return m_v[i];
+		} else {
+			cerr << "Errore : indice " << i << ", dimensione " << m_N << endl;
+			exit(-1);
+		}
+	}; 
+
+	void ExchangeComponents(unsigned int i, unsigned int j){
+
+		T temp = GetComponent(i);
+		
+		SetComponent(i, GetComponent(k));
+		SetComponent(k, temp);
+	};
+
+	T& operator[] (unsigned int i){
+
+		if (i<m_N) {
+			return m_v[i]; 
+		} else {
+			cerr << "Errore : indice " << i << ", dimensione " << m_N << endl;
+			exit(-1);
+		}
+	}; 
+
+private:
+	unsigned int m_N; // dimensione
+	T* m_v; // tipo generico T
+};
+
+#endif // __Vettore_h__
+
